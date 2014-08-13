14bbe8a8bd4fdf1383b88a4b7aea9eae5d1b1baa
Justin R. Miller <incanus@codesorcery.net>
2014-08-13 10:56:41 -0700

    added Fingertips for ObjC bridging

--- Swift Testing/AppDelegate.swift
+++ Swift Testing/AppDelegate.swift
@@ -7,6 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        let newWindow = MBFingerTipWindow(frame: self.window!.frame)
+        newWindow.rootViewController = self.window!.rootViewController
+        self.window = newWindow
+
         return true
     }
 }
