bc1d7969e4b2e38e0c36ed1b12c976b9d1059fd0
Justin R. Miller <incanus@codesorcery.net>
2014-08-13 11:37:22 -0700

    basic GL integration

--- Swift Testing-Bridging-Header.h
+++ Swift Testing-Bridging-Header.h
@@ -1,1 +1,3 @@
 #import "MBFingerTipWindow.h"
+
+#import "MGLMapView.h"
--- Swift Testing/ViewController.swift
+++ Swift Testing/ViewController.swift
@@ -2,9 +2,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var mapView: MGLMapView?
+    let accessToken = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor.orangeColor()
+        let styleJSON = NSString.stringWithContentsOfFile(NSBundle.mainBundle().pathForResource("style", ofType: "json"),
+                                                          encoding: NSString.defaultCStringEncoding(),
+                                                          error: nil)
+
+        mapView = MGLMapView(frame: view.bounds,
+                             styleJSON: styleJSON,
+                             accessToken: accessToken)
+
+        view.addSubview(mapView!)
+
+        mapView!.setCenterCoordinate(CLLocationCoordinate2DMake(46.049900, -122.095678),
+                 zoomLevel: 12,
+                 animated: false)
     }
 }
