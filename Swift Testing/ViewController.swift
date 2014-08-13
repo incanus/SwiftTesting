3452160b5cea2fc5042cc9a2a63964d4595825c3
Justin R. Miller <incanus@codesorcery.net>
2014-08-13 14:38:40 -0700

    use new bundled styles API

--- Swift Testing/ViewController.swift
+++ Swift Testing/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
                 zoomLevel: 12,
                 animated: false)
 
+        mapView.useBundledStyleNamed("outdoors")
+
         view.addSubview(mapView)
     }
 }
